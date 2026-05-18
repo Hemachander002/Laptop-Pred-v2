9a0b7a1f735f35b1fbc90b8e5710ce9ca1d027ac
Hemachander002 <sathyameena75@gmail.com>
2026-05-18 16:08:38 +0530

    minor changes in main.py

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from src.laptop_price_prediction.pipeline.data_validation_pipeline import DataVa
 from src.laptop_price_prediction import logger
 from src.laptop_price_prediction.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-
+STAGE_NAME="Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj=DataIngestionTrainingPipeline()
+    obj.InitiateDataIngestionTrainingPipeline()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 STAGE_NAME="Data Validation Stage"
 try:
